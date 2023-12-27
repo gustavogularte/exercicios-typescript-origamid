8000ae8e8b2589e931f46f80e874bfcd71c7bfa1
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-12-27 18:20:59 -0300

    ex 10

--- script.js
+++ script.js
@@ -163,3 +163,36 @@ function arredondar(valor) {
         return Math.ceil(+valor).toString();
     }
 }
+//Exercício 10
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+async function fetchAPI() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const json = await response.json();
+    handleCurso(json);
+}
+fetchAPI();
+function isCurso(curso) {
+    if (curso &&
+        typeof curso === 'object' &&
+        'nome' in curso &&
+        'horas' in curso &&
+        'tags' in curso) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function handleCurso(item) {
+    if (Array.isArray(item)) {
+        item.filter(isCurso).forEach((item) => {
+            document.body.innerHTML += `
+      <h2>${item.nome}</h2>
+      `;
+        });
+    }
+}
--- script.ts
+++ script.ts
@@ -224,3 +224,51 @@ function arredondar(valor: string | number): string | number {
     return Math.ceil(+valor).toString();
   }
 }
+
+//Exercício 10
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+async function fetchAPI() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const json = await response.json();
+  handleCurso(json);
+}
+
+fetchAPI();
+
+interface Curso2 {
+  nome: string;
+  horas: number;
+  aulas: number;
+  gratuito: boolean;
+  tags: string[];
+  idAulas: number[];
+  nivel: 'iniciante' | 'avancado';
+}
+
+function isCurso(curso: unknown): curso is Curso2 {
+  if (
+    curso &&
+    typeof curso === 'object' &&
+    'nome' in curso &&
+    'horas' in curso &&
+    'tags' in curso
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleCurso(item: unknown) {
+  if (Array.isArray(item)) {
+    item.filter(isCurso).forEach((item) => {
+      document.body.innerHTML += `
+      <h2>${item.nome}</h2>
+      `;
+    });
+  }
+}
