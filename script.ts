4fb391d6081f0fe723881580429f6121fe66b7b7
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2024-01-26 23:07:35 -0300

    ex 3

--- script.ts
+++ script.ts
@@ -6,6 +6,7 @@ function normalizarTexto(texto: string) {
   return texto.trim().toLowerCase();
 }
 
+
 //Conserte as funções com TypeScript
 // const input = document.querySelector('input');
 // const total = localStorage.getItem('total');
@@ -45,3 +46,19 @@ function totalMudou() {
 }
 
 input?.addEventListener('keyup', totalMudou);
+
+
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+function toNumber(value: number | string) {
+  if (typeof value === 'number') {
+    return value;
+  } else if (typeof value === 'string') {
+    return Number(value);
+  } else {
+    throw 'value deve ser um número ou uma string';
+  }
+}
