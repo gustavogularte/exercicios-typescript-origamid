dde34a3880c529a01bcca1351258dbd3cda58fb1
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2024-02-01 23:34:31 -0300

    .

--- script.js
+++ script.js
@@ -136,3 +136,35 @@ function arredondarCima(valor) {
         return `${Math.ceil(+valor)}`;
     }
 }
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+async function fetchCursos2() {
+    const response = await fetch('https://api.origamid.dev/json/cursos.json');
+    const json = await response.json();
+    mostrarCursos2(json);
+}
+fetchCursos2();
+function isCurso(item) {
+    if (item &&
+        typeof item === 'object' &&
+        'nome' in item &&
+        'horas' in item &&
+        'tags' in item) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function mostrarCursos2(data) {
+    if (Array.isArray(data)) {
+        data.forEach((item) => {
+            if (isCurso(item)) {
+                console.log(item.horas);
+            }
+        });
+    }
+}
--- script.ts
+++ script.ts
@@ -110,9 +110,9 @@ interface Cursos {
 function mostrarCursos(cursos: Cursos[]) {
   cursos.forEach((curso) => {
     const color = curso.nivel === 'iniciante' ? 'blue' : 'red';
-  //   document.body.innerHTML += `
-  //    <h1 style="color: ${color};">${curso.nome}</h1>
-  // `;
+    //   document.body.innerHTML += `
+    //    <h1 style="color: ${color};">${curso.nome}</h1>
+    // `;
   });
 }
 
@@ -167,7 +167,6 @@ function toggleMenu(e: PointerEvent) {
 }
 button?.addEventListener('pointerdown', toggleMenu);
 
-
 // Crie uma função que arredonda um valor passado para cima.
 // A função pode receber string ou number.
 // A função deve retornar o mesmo tipo que ela receber.
@@ -178,5 +177,47 @@ function arredondarCima(valor: number | string): number | string {
     return Math.ceil(valor);
   } else {
     return `${Math.ceil(+valor)}`;
+  }
+}
+
+// 1 - Faça um fetch da API: https://api.origamid.dev/json/cursos.json
+// 2 - Defina a interface da API
+// 3 - Crie um Type Guard, que garanta que a API possui nome, horas e tags
+// 4 - Use Type Guards para garantir a Type Safety do código
+// 5 - Preencha os dados da API na tela.
+async function fetchCursos2() {
+  const response = await fetch('https://api.origamid.dev/json/cursos.json');
+  const json = await response.json();
+  mostrarCursos2(json);
+}
+fetchCursos2();
+
+interface Curso {
+  nome: string;
+  horas: number;
+  tags: string[];
+}
+
+function isCurso(item: unknown): item is Curso {
+  if (
+    item &&
+    typeof item === 'object' &&
+    'nome' in item &&
+    'horas' in item &&
+    'tags' in item
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function mostrarCursos2(data: any) {
+  if (Array.isArray(data)) {
+    data.forEach((item) => {
+      if (isCurso(item)) {
+        console.log(item.horas);
+      }
+    });
   }
 }
