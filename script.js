ac90024781a8b8ab1a2b25f4cfc46de1dc90459a
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2024-02-03 01:40:22 -0300

    ex 9

--- script.js
+++ script.js
@@ -212,3 +212,18 @@ function handleKeyup({ target }) {
 }
 const form = document.querySelector('#form');
 form?.addEventListener('keyup', handleKeyup);
+// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
+async function fetchVendas() {
+    const response = await fetch('https://api.origamid.dev/json/vendas.json');
+    const json = await response.json();
+    totalVendas(json);
+}
+fetchVendas();
+function totalVendas(vendas) {
+    vendas.reduce((total, venda) => {
+        //console.log(total + venda[1]);
+        return total + venda[1];
+    }, 0);
+}
